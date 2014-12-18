42d832425082bd5ad2cd78f1cf027a9590504649
teckademic <zacharyv@gmail.com>
2014-12-18 11:46:38 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,23 @@
-<?php
-	// set some variables w/ database login information
-	$hostname = "localhost";
-	$database_name = "tecka_ugather";
-	$username = "tecka_ugather";
-	$password = "ugather";
-	$eventtable = "Event";
-	
-	// open and connect to the database
-	$ugather = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
-	mysql_select_db($database_name)or die("Error: cannot select the chosen database!");
+<?php
+
+	// set some variables w/ database login information
+
+	$hostname = "localhost";
+
+	$database_name = "";
+
+	$username = "";
+
+	$password = "";
+
+	$eventtable = "Event";
+
+	
+
+	// open and connect to the database
+
+	$ugather = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
+
+	mysql_select_db($database_name)or die("Error: cannot select the chosen database!");
+
 ?>
